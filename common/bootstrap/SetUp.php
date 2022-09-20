64e9ab04570bb03deb19a0afec025f8a69a1be08
Nik.Brosko <Nikita.brosko@mail.ru>
2022-09-20 20:59:11 +0300

    Optimizing declaration of services in dependency container

--- common/bootstrap/SetUp.php
+++ common/bootstrap/SetUp.php
@@ -6,22 +6,28 @@ use frontend\services\auth\PasswordResetService;
 use frontend\services\auth\SignupService;
 use frontend\services\contact\ContactService;
 use yii\base\BootstrapInterface;
+use yii\di\Instance;
+use yii\mail\MailerInterface;
 
 class SetUp implements BootstrapInterface {
 
     public function bootstrap($app) {
         $container = \Yii::$container;
 
+        $container->setSingleton(MailerInterface::class, function () use ($app) {
+            return $app->mailer;
+        });
+
         $container->setSingleton(SignupService::class, [], [
-            $app->mailer
+            Instance::of(MailerInterface::class)
         ]);
 
         $container->setSingleton(PasswordResetService::class, [], [
-            $app->mailer
+            Instance::of(MailerInterface::class)
         ]);
 
         $container->setSingleton(ContactService::class, [], [
-            $app->mailer
+            Instance::of(MailerInterface::class)
         ]);
     }
 }
