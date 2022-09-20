23550169b4031b5febd2e9e7aff3caa069cd1e4e
Nik.Brosko <Nikita.brosko@mail.ru>
2022-09-20 20:24:49 +0300

    Fix bug with cookies

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -2,5 +2,5 @@
 
 return [
     'cookieValidationKey' => '',
-    'cookieDomain' => '.shop.dev',
+    'cookieDomain' => '',
 ];
--- environments/index.php
+++ environments/index.php
@@ -44,6 +44,7 @@ return [
         ],
         'setCookieValidationKey' => [
             'common/config/params-local.php',
+            'common/config/codeception-local.php'
         ],
     ],
     'Production' => [
