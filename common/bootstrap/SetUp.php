a5b30555b872136be74022044f5ccacd3b3187ad
Nik.Brosko <Nikita.brosko@mail.ru>
2022-09-20 20:25:15 +0300

    Implement dependency container

--- common/bootstrap/SetUp.php
+++ common/bootstrap/SetUp.php
@@ -2,11 +2,26 @@
 
 namespace common\bootstrap;
 
+use frontend\services\auth\PasswordResetService;
+use frontend\services\auth\SignupService;
+use frontend\services\contact\ContactService;
 use yii\base\BootstrapInterface;
 
 class SetUp implements BootstrapInterface {
 
     public function bootstrap($app) {
-        // TODO: Implement bootstrap() method.
+        $container = \Yii::$container;
+
+        $container->setSingleton(SignupService::class, [], [
+            $app->mailer
+        ]);
+
+        $container->setSingleton(PasswordResetService::class, [], [
+            $app->mailer
+        ]);
+
+        $container->setSingleton(ContactService::class, [], [
+            $app->mailer
+        ]);
     }
 }
