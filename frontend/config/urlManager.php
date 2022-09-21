1baa1fa461bb934b08786f894d019d96cb4d1230
Nik.Brosko <Nikita.brosko@mail.ru>
2022-09-21 15:19:17 +0300

    Update urlManager in frontend

--- frontend/config/urlManager.php
+++ frontend/config/urlManager.php
@@ -6,7 +6,12 @@ return [
     'showScriptName' => false,
     'rules' => [
         '' => 'site/index',
-        '<_a:login|logout>' => 'site/<_a>',
+        '<_a:about>' => 'site/<_a>',
+        '<_a:contact>' => 'contact/<_a>',
+        '<_a:signup>' => 'auth/signup/<_a>',
+        'signup/<_a:[\w-]+>' => 'auth/signup/<_a>',
+        '<_a:login|logout>' => 'auth/auth/<_a>',
+
         '<_c:[\w\-]+>' => '<_c>/index',
         '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
         '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
