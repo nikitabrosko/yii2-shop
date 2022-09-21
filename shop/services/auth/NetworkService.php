954fc8011cdc739ed1b8c9a95744120692a82c8f
Nik.Brosko <Nikita.brosko@mail.ru>
2022-09-21 21:14:49 +0300

    Add attach method to NetworkService class

--- shop/services/auth/NetworkService.php
+++ shop/services/auth/NetworkService.php
@@ -26,4 +26,27 @@ class NetworkService
 
         return $user;
     }
+
+    public function attach($id, $network, $identity)
+    {
+        if (User::findByNetworkIdentity($network, $identity)) {
+            throw new \DomainException('Network is already attached.');
+        }
+
+        $user = User::findOne([
+            'id' => $id,
+        ]);
+
+        $user->attachNetwork($network, $identity);
+
+        if (!$user->save()) {
+            throw new \DomainException('Sorry, we are unable to sign you up.');
+        }
+
+        foreach ($user->networks as $network) {
+            $network->user_id = $user->getId();
+
+            $network->save();
+        }
+    }
 }
