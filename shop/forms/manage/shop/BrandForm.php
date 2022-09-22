1059745064089f7efcef2fd3ef63b9148621e15d
Nik.Brosko <Nikita.brosko@mail.ru>
2022-09-22 18:34:00 +0300

    Create BrandForm and MetaForm model forms

---
+++ shop/forms/manage/shop/BrandForm.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace shop\forms\manage\shop;
+
+use shop\entities\shop\Brand;
+use shop\forms\CompositeForm;
+use shop\forms\manage\MetaForm;
+
+/**
+ * @property MetaForm $meta
+ */
+class BrandForm extends CompositeForm
+{
+    public $name;
+    public $slug;
+
+    private $_brand;
+
+    public function __construct(Brand $brand = null, $config = [])
+    {
+        if ($brand) {
+            $this->name = $brand->name;
+            $this->slug = $brand->slug;
+            $this->meta = new MetaForm($brand->meta);
+            $this->_brand = $brand;
+        } else {
+            $this->meta = new MetaForm();
+        }
+
+        parent::__construct($config);
+    }
+
+    public function rules() : array
+    {
+        return [
+            [['name', 'slug'], 'required'],
+            [['name', 'slug'], 'string', 'max' => 255],
+            ['slug', 'match', 'pattern' => '#^[a-z0-9_-]*$#s'],
+            [['name', 'slug'], 'unique', 'targetClass' => Brand::class, 'filter' => $this->_brand ? ['<>', 'id', $this->_brand->id] : null],
+        ];
+    }
+
+    protected function internalForms(): array
+    {
+        return ['meta'];
+    }
+}
