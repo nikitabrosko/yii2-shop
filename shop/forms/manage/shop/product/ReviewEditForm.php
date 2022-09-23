3e642700f7fed7b227019bd9efc6f895ffd8c3c1
Nik.Brosko <Nikita.brosko@mail.ru>
2022-09-23 18:50:09 +0300

    Create ReviewEditForm model

---
+++ shop/forms/manage/shop/product/ReviewEditForm.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace shop\forms\manage\shop\product;
+
+use shop\entities\shop\product\Review;
+use yii\base\Model;
+
+class ReviewEditForm extends Model
+{
+    public $vote;
+    public $text;
+
+    public function __construct(Review $review, $config = [])
+    {
+        $this->vote = $review->vote;
+        $this->text = $review->text;
+
+        parent::__construct($config);
+    }
+
+    public function rules() : array
+    {
+        return [
+            [['vote', 'text'], 'required'],
+            ['vote', 'in', 'range' => [1, 2, 3, 4, 5]],
+            ['text', 'string'],
+        ];
+    }
+}
