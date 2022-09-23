0ba863c05bb29403b3043730f209cb52ead1ae0f
Nik.Brosko <Nikita.brosko@mail.ru>
2022-09-23 11:47:58 +0300

    Create new migration, that creates shop_categories table in database

---
+++ console/migrations/m220923_084322_create_shop_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%shop_categories}}`.
+ */
+class m220923_084322_create_shop_categories_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+
+        $this->createTable('{{%shop_categories}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'slug' => $this->string()->notNull(),
+            'title' => $this->string(),
+            'description' => $this->string(),
+            'meta_json' => 'JSON NOT NULL',
+            'lft' => $this->integer()->notNull(),
+            'rgt' => $this->integer()->notNull(),
+            'depth' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%shop_categories}}');
+    }
+}
