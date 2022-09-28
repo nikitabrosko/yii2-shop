7bd5f5d16c3e3965e43e25d3f7736ff70901f83a
Nik.Brosko <Nikita.brosko@mail.ru>
2022-09-28 12:19:55 +0300

    Update migration for creating categories table

--- console/migrations/m220923_084322_create_shop_categories_table.php
+++ console/migrations/m220923_084322_create_shop_categories_table.php
@@ -19,12 +19,26 @@ class m220923_084322_create_shop_categories_table extends Migration
             'name' => $this->string()->notNull(),
             'slug' => $this->string()->notNull(),
             'title' => $this->string(),
-            'description' => $this->string(),
+            'description' => $this->text(),
             'meta_json' => 'JSON NOT NULL',
             'lft' => $this->integer()->notNull(),
             'rgt' => $this->integer()->notNull(),
             'depth' => $this->integer()->notNull(),
         ], $tableOptions);
+
+        $this->createIndex('{{%idx-shop_categories-slug}}', '{{%shop_categories}}', 'slug', true);
+
+        $this->insert('{{%shop_categories}}', [
+            'id' => 1,
+            'name' => '',
+            'slug' => 'root',
+            'title' => null,
+            'description' => null,
+            'meta_json' => '{}',
+            'lft' => 1,
+            'rgt' => 2,
+            'depth' => 0,
+        ]);
     }
 
     /**
