2d995fd229b9fbb71599ca5eb5be60a620965468
Nik.Brosko <Nikita.brosko@mail.ru>
2022-09-28 11:50:22 +0300

    Update urlManager with managing shop/ paths

--- backend/config/urlManager.php
+++ backend/config/urlManager.php
@@ -11,5 +11,11 @@ return [
         '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
         '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
         '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
+
+        'shop' => 'shop/default/cabinet',
+        'shop/<_c:[\w\-]+>' => 'shop/<_c>/index',
+        'shop/<_c:[\w\-]+>/<id:\d+>' => 'shop/<_c>/view',
+        'shop/<_c:[\w\-]+>/<_a:[\w-]+>' => 'shop/<_c>/<_a>',
+        'shop/<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => 'shop/<_c>/<_a>',
     ],
 ];
