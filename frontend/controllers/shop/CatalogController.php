de2cf9c83fcf35ace437828d94f7488721097eec
Nik.Brosko <Nikita.brosko@mail.ru>
2022-09-30 17:44:26 +0300

    Create CatalogController (unstable version)

---
+++ frontend/controllers/shop/CatalogController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace frontend\controllers\shop;
+
+use shop\entities\shop\product\Product;
+use shop\readModels\shop\BrandReadRepository;
+use shop\readModels\shop\CategoryReadRepository;
+use shop\readModels\shop\ProductReadRepository;
+use shop\readModels\shop\TagReadRepository;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+
+class CatalogController extends Controller
+{
+    public $layout = 'catalog';
+
+    private $products;
+    private $categories;
+    private $brands;
+    private $tags;
+
+    public function __construct(
+        $id,
+        $module,
+        ProductReadRepository $products,
+        CategoryReadRepository $categories,
+        BrandReadRepository $brands,
+        TagReadRepository $tags,
+        $config = []
+    )
+    {
+        parent::__construct($id, $module, $config);
+        $this->products = $products;
+        $this->categories = $categories;
+        $this->brands = $brands;
+        $this->tags = $tags;
+    }
+
+    public function actionIndex()
+    {
+        $dataProvider = $this->products->getAll();
+        $category = $this->categories->getRoot();
+
+        return $this->render('index', [
+            'category' => $category,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionCategory($id)
+    {
+        if (!$category = $this->categories->find($id)) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+
+        $dataProvider = $this->products->getAllByCategory($category);
+
+        return $this->render('category', [
+            'category' => $category,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionBrand($id)
+    {
+        if (!$brand = $this->brands->find($id)) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+
+        $dataProvider = $this->products->getAllByBrand($brand);
+
+        return $this->render('brand', [
+            'brand' => $brand,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionTag($id)
+    {
+        if (!$tag = $this->tags->find($id)) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+
+        $dataProvider = $this->products->getAllByTag($tag);
+
+        return $this->render('tag', [
+            'tag' => $tag,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionProduct($id)
+    {
+        if (!$product = $this->products->find($id)) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+
+        return $this->render('product', [
+            'product' => $product,
+        ]);
+    }
+}
