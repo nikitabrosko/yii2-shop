094f1c1fc97f5e2663e359ae2312ecbaf0b0f4f9
Nik.Brosko <Nikita.brosko@mail.ru>
2022-10-01 13:58:18 +0300

    Create CategoryReadRepository (test version)

--- shop/readModels/shop/CategoryReadRepository.php
+++ shop/readModels/shop/CategoryReadRepository.php
@@ -3,7 +3,9 @@
 namespace shop\readModels\shop;
 
 use shop\entities\shop\Category;
+use shop\readModels\shop\views\CategoryView;
 use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
 
 class CategoryReadRepository
 {
@@ -20,5 +22,24 @@ class CategoryReadRepository
             ->andWhere(['id' => $id])
             ->andWhere(['>', 'depth', 0])
             ->one();
+    }
+
+    public function getTreeWithSubsOf(Category $category = null): array
+    {
+        $query = Category::find()->andWhere(['>', 'depth', 0])->orderBy('lft');
+
+        if ($category) {
+            $criteria = ['or', ['depth' => 1]];
+            foreach (ArrayHelper::merge([$category], $category->parents) as $item) {
+                $criteria[] = ['and', ['>', 'lft', $item->lft], ['<', 'rgt', $item->rgt], ['depth' => $item->depth + 1]];
+            }
+            $query->andWhere($criteria);
+        } else {
+            $query->andWhere(['depth' => 1]);
+        }
+
+        return array_map(function (Category $category) {
+            return new CategoryView($category, 5);
+        }, $query->all());
     }
 }
