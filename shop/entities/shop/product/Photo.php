f5a8fdedad7a98a00cddf76c6e4e0f738faeb8cf
Nik.Brosko <Nikita.brosko@mail.ru>
2022-10-01 19:01:08 +0300

    Add new profiles to photos thumbs

--- shop/entities/shop/product/Photo.php
+++ shop/entities/shop/product/Photo.php
@@ -53,6 +53,9 @@ class Photo extends ActiveRecord
                     'admin' => ['width' => 100, 'height' => 70],
                     'thumb' => ['width' => 640, 'height' => 480],
                     'catalog_list' => ['width' => 250, 'height' => 250],
+                    'catalog_product_main' => ['width' => 750, 'height' => 1000],
+                    'catalog_product_additional' => ['width' => 66, 'height' => 66],
+                    'catalog_origin' => ['width' => 1024, 'height' => 768],
                 ],
             ],
         ];
