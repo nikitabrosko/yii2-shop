0e2c083042c695c376b59e8dd7b9876ab283ac41
Nik.Brosko <Nikita.brosko@mail.ru>
2022-10-07 15:45:07 +0300

    Refactor and add findBySlug method in CategoryReadRepository class

--- shop/readModels/shop/CategoryReadRepository.php
+++ shop/readModels/shop/CategoryReadRepository.php
@@ -9,19 +9,24 @@ use yii\helpers\ArrayHelper;
 
 class CategoryReadRepository
 {
-    public function getRoot()
+    public function getRoot() : ?Category
     {
         return Category::find()
             ->roots()
             ->one();
     }
 
-    public function find($id)
+    public function find($id) : ?Category
     {
         return Category::find()
             ->andWhere(['id' => $id])
             ->andWhere(['>', 'depth', 0])
             ->one();
+    }
+
+    public function findBySlug($slug) : ?Category
+    {
+        return Category::find()->andWhere(['slug' => $slug])->andWhere(['>', 'depth', 0])->one();
     }
 
     public function getTreeWithSubsOf(Category $category = null): array
@@ -30,16 +35,18 @@ class CategoryReadRepository
 
         if ($category) {
             $criteria = ['or', ['depth' => 1]];
+
             foreach (ArrayHelper::merge([$category], $category->parents) as $item) {
                 $criteria[] = ['and', ['>', 'lft', $item->lft], ['<', 'rgt', $item->rgt], ['depth' => $item->depth + 1]];
             }
+
             $query->andWhere($criteria);
         } else {
             $query->andWhere(['depth' => 1]);
         }
 
         return array_map(function (Category $category) {
-            return new CategoryView($category, 5);
+            return new CategoryView($category, 0);
         }, $query->all());
     }
 }
