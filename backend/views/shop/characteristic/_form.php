6b7ec1339cd31de93e5c9fb867830f53a602c3d7
Nik.Brosko <Nikita.brosko@mail.ru>
2022-10-08 21:19:52 +0300

    Fix characteristic _form view

--- backend/views/shop/characteristic/_form.php
+++ backend/views/shop/characteristic/_form.php
@@ -4,7 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /** @var yii\web\View $this */
-/** @var shop\entities\shop\Characteristic $model */
+/** @var shop\forms\manage\shop\CharacteristicForm $model */
 /** @var yii\widgets\ActiveForm $form */
 ?>
 
