661bd186ef1196925117056304a28caa71a6e796
Nik.Brosko <Nikita.brosko@mail.ru>
2022-10-11 14:49:28 +0300

    Create WishlistController and wishlist view

---
+++ frontend/controllers/cabinet/WishlistController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace frontend\controllers\cabinet;
+
+use shop\readModels\shop\ProductReadRepository;
+use shop\services\cabinet\WishlistService;
+use Yii;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+
+class WishlistController extends Controller
+{
+    public $layout = 'cabinet';
+
+    private $wishlistService;
+    private $products;
+
+    public function __construct($id, $module, WishlistService $wishlistService,
+                                ProductReadRepository $products, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+
+        $this->wishlistService = $wishlistService;
+        $this->products = $products;
+    }
+
+    public function behaviors() : array
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'add' => ['POST'],
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    public function actionWishlist()
+    {
+        $dataProvider = $this->products->getWishList(\Yii::$app->user->id);
+
+        return $this->render('wishlist', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionAdd($id)
+    {
+        try {
+            $this->wishlistService->add(Yii::$app->user->id, $id);
+
+            Yii::$app->session->setFlash('success', 'Success!');
+        } catch (\DomainException $e) {
+            Yii::$app->errorHandler->logException($e);
+            Yii::$app->session->setFlash('error', $e->getMessage());
+        }
+
+        return $this->redirect(Yii::$app->request->referrer ?: ['wishlist']);
+    }
+
+    public function actionDelete($id)
+    {
+        try {
+            $this->wishlistService->remove(Yii::$app->user->id, $id);
+        } catch (\DomainException $e) {
+            Yii::$app->errorHandler->logException($e);
+            Yii::$app->session->setFlash('error', $e->getMessage());
+        }
+
+        return $this->redirect(['wishlist']);
+    }
+}
