6af02cad296640844f95c47c4cbe86905a42f330
Nik.Brosko <Nikita.brosko@mail.ru>
2022-10-11 14:46:50 +0300

    Add new photo profile

--- shop/entities/shop/product/Photo.php
+++ shop/entities/shop/product/Photo.php
@@ -57,6 +57,7 @@ class Photo extends ActiveRecord
                     'catalog_product_main' => ['processor' => [new WaterMarker(750, 1000, '@frontend/web/img/logo.png'), 'process']],
                     'catalog_product_additional' => ['width' => 66, 'height' => 66],
                     'catalog_origin' => ['processor' => [new WaterMarker(1024, 768, '@frontend/web/img/logo.png'), 'process']],
+                    'wishlist' => ['width' => 100, 'height' => 100],
                 ],
             ],
         ];
