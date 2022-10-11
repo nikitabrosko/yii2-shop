0ac34656441f8a0ee669964b98c1599a1e459a58
Nik.Brosko <Nikita.brosko@mail.ru>
2022-10-11 20:30:44 +0300

    Create Cart entities

---
+++ shop/cart/Cart.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace shop\cart;
+
+use shop\cart\cost\calculator\CalculatorInterface;
+use shop\cart\cost\Cost;
+use shop\cart\storage\StorageInterface;
+use shop\exceptions\NotFoundException;
+
+class Cart
+{
+    /**
+     * @var CartItem[]
+     */
+    private $items;
+    private $storage;
+    private $calculator;
+
+    public function __construct(StorageInterface $storage, CalculatorInterface $calculator)
+    {
+        $this->storage = $storage;
+        $this->calculator = $calculator;
+        $this->loadItems();
+    }
+
+    public function getItems() : array
+    {
+        $this->loadItems();
+
+        return $this->items;
+    }
+
+    public function getAmount() : int
+    {
+        $this->loadItems();
+
+        return count($this->items);
+    }
+
+    public function getCost() : Cost
+    {
+        $this->loadItems();
+
+        return $this->calculator->getCost($this->items);
+    }
+
+    public function add(CartItem $cartItem)
+    {
+        $this->loadItems();
+
+        foreach ($this->items as $i => $item) {
+            if ($item->getId() == $cartItem->getId()) {
+                $item->changeQuantity($item->getQuantity() + $cartItem->getQuantity());
+                $this->saveItems();
+
+                return;
+            }
+        }
+
+        $this->items[] = $cartItem;
+
+        $this->saveItems();
+    }
+
+    public function set($id, $quantity)
+    {
+        $this->loadItems();
+
+        foreach ($this->items as $i => $item) {
+            if ($item->getId() == $id) {
+                $this->items[$i]->changeQuantity($quantity);
+
+                $this->saveItems();
+
+                return;
+            }
+        }
+
+        throw new NotFoundException('Item not found.');
+    }
+
+    public function remove($id)
+    {
+        $this->loadItems();
+
+        foreach ($this->items as $i => $item) {
+            if ($item->getId() == $id) {
+                unset($this->items[$i]);
+            }
+        }
+
+        $this->saveItems();
+    }
+
+    public function clear()
+    {
+        $this->items = [];
+
+        $this->saveItems();
+    }
+
+    private function loadItems()
+    {
+        if ($this->items === null) {
+            $this->items = $this->storage->load();
+        }
+    }
+
+    private function saveItems()
+    {
+        $this->storage->save($this->items);
+    }
+}
---
+++ shop/cart/CartItem.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace shop\cart;
+
+use shop\entities\shop\product\Modification;
+use shop\entities\shop\product\Product;
+
+class CartItem
+{
+    private $product;
+    private $modificationId;
+    private $quantity;
+
+    public function __construct(Product $product, $modificationId, int $quantity)
+    {
+        $this->product = $product;
+        $this->modificationId = $modificationId;
+        $this->quantity = $quantity;
+    }
+
+    public function getProduct(): Product
+    {
+        return $this->product;
+    }
+
+    public function getModification() : ?Modification
+    {
+        if ($this->modificationId) {
+            return $this->product->getModification($this->modificationId);
+        }
+
+        return null;
+    }
+
+    public function getQuantity() : int
+    {
+        return $this->quantity;
+    }
+
+    public function getId() : string
+    {
+        return md5(serialize([$this->product->id, $this->modificationId]));
+    }
+
+    public function changeQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+    }
+
+    public function getPrice() : float
+    {
+        if ($this->modificationId) {
+            return $this->product->getModification($this->modificationId)->price;
+        }
+
+        return $this->product->price_new;
+    }
+
+    public function getCost() : float
+    {
+        return $this->getPrice() * $this->quantity;
+    }
+}
