31b843666326f36983543f8ffa966d1a1ca236fa
Nik.Brosko <Nikita.brosko@mail.ru>
2022-10-13 12:20:36 +0300

    Refactor and fix _product and featured views

--- frontend/views/shop/catalog/_product.php
+++ frontend/views/shop/catalog/_product.php
@@ -1,6 +1,7 @@
 <?php
 
 /* @var $this yii/web/View */
+
 /* @var $product shop\entities\shop\product\Product */
 
 use shop\helpers\PriceHelper;
@@ -13,36 +14,36 @@ $url = Url::to(['product', 'id' => $product->id]);
 ?>
 
 <div class="col">
-    <form method="post" data-oc-toggle="ajax" data-oc-load="" data-oc-target="#header-cart">
-        <div class="product-thumb">
-            <? if ($product->mainPhoto): ?>
-                <div class="image">
-                    <a href="<?= Html::encode($url) ?>">
-                        <img src="<?= Html::encode($product->mainPhoto->getThumbFileUrl('file', 'catalog_list')) ?>"
-                             alt=""
-                             class="img-fluid" />
-                    </a>
-                </div>
-            <? endif; ?>
-            <div class="content">
-                <div class="description">
-                    <h4><a href="<?= Html::encode($url) ?>"><?= Html::encode($product->name) ?></a></h4>
-                    <p><?= Html::encode(StringHelper::truncateWords(strip_tags($product->description), 20)) ?></p>
-                    <div class="price">
-                        <span class="price-new">BYN <?= Html::encode(PriceHelper::format($product->price_new)) ?></span>
-                        <? if ($product->price_old): ?>
-                            <span class="price-old">BYN <?= Html::encode(PriceHelper::format($product->price_old)) ?></span>
-                        <? endif; ?>
-                    </div>
-                </div>
-                <div class="button-group">
-                    <button type="submit" formaction="<?= Html::encode(Url::to(['shop/cart/add', 'id' => $product->id])) ?>" data-bs-toggle="tooltip" title="Add to Cart"><i class="fas fa-shopping-cart"></i></button>
-                    <button type="submit" formaction="<?= Html::encode(Url::to(['cabinet/wishlist/add', 'id' => $product->id])) ?>" data-bs-toggle="tooltip" title="Add to Wish List"><i class="fas fa-heart"></i></button>
-                    <button type="submit" formaction="" data-bs-toggle="tooltip" title="Compare this Product"><i class="fas fa-exchange-alt"></i></button>
+    <div class="product-thumb">
+        <? if ($product->mainPhoto): ?>
+            <div class="image">
+                <a href="<?= Html::encode($url) ?>">
+                    <img src="<?= Html::encode($product->mainPhoto->getThumbFileUrl('file', 'catalog_list')) ?>"
+                         alt=""
+                         class="img-fluid"/>
+                </a>
+            </div>
+        <? endif; ?>
+        <div class="content">
+            <div class="description">
+                <h4><a href="<?= Html::encode($url) ?>"><?= Html::encode($product->name) ?></a></h4>
+                <p><?= Html::encode(StringHelper::truncateWords(strip_tags($product->description), 20)) ?></p>
+                <div class="price">
+                    <span class="price-new">BYN <?= Html::encode(PriceHelper::format($product->price_new)) ?></span>
+                    <? if ($product->price_old): ?>
+                        <span class="price-old">BYN <?= Html::encode(PriceHelper::format($product->price_old)) ?></span>
+                    <? endif; ?>
                 </div>
             </div>
-            <input type="hidden" name="product_id" value="42" />
-            <input type="hidden" name="quantity" value="2" />
+            <div class="button-group">
+                <button type="button" href="<?= Html::encode(Url::to(['shop/cart/add', 'id' => $product->id])) ?>"
+                        data-method="post" title="Add to Cart"><i class="fas fa-shopping-cart"></i></button>
+                <button type="button"
+                        href="<?= Html::encode(Url::to(['cabinet/wishlist/add', 'id' => $product->id])) ?>"
+                        data-method="post" title="Add to Wish List"><i class="fas fa-heart"></i></button>
+                <button type="submit" formaction="" data-bs-toggle="tooltip" title="Compare this Product"><i
+                            class="fas fa-exchange-alt"></i></button>
+            </div>
         </div>
-    </form>
+    </div>
 </div>
