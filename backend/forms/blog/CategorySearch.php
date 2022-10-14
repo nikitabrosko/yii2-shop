918992cbb04a54bf10f3c854429d957f40fe4537
Nik.Brosko <Nikita.brosko@mail.ru>
2022-10-14 16:38:23 +0300

    Create Category entity and CRUD for blog

---
+++ backend/forms/blog/CategorySearch.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace backend\forms\blog;
+
+use shop\entities\blog\Category;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+
+class CategorySearch extends Model
+{
+    public $id;
+    public $name;
+    public $slug;
+    public $title;
+
+    public function rules(): array
+    {
+        return [
+            [['id'], 'integer'],
+            [['name', 'slug', 'title'], 'safe'],
+        ];
+    }
+
+    public function search(array $params): ActiveDataProvider
+    {
+        $query = Category::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'sort' => [
+                'defaultOrder' => ['sort' => SORT_ASC]
+            ]
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+        ]);
+
+        $query
+            ->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'slug', $this->slug])
+            ->andFilterWhere(['like', 'title', $this->title]);
+
+        return $dataProvider;
+    }
+}
--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -47,6 +47,7 @@
                     ]],
                     ['label' => 'Blog', 'icon' => 'folder', 'items' => [
                         ['label' => 'Tags', 'icon' => 'file-o', 'url' => ['blog/tag/index'], 'active' => $this->context->id == 'blog/tag/index'],
+                        ['label' => 'Category', 'icon' => 'file-o', 'url' => ['blog/category/index'], 'active' => $this->context->id == 'blog/category/index'],
                     ]],
                 ],
             ]
