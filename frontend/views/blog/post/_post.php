e1975dbd7fbd5da5417ef8e93c52cbf9eca95c33
Nik.Brosko <Nikita.brosko@mail.ru>
2022-10-18 15:29:24 +0300

    Change _post view

--- frontend/views/blog/post/_post.php
+++ frontend/views/blog/post/_post.php
@@ -9,16 +9,18 @@ use yii\helpers\Url;
 $url = Url::to(['post', 'id' =>$model->id]);
 ?>
 
-<div class="blog-posts-item">
+<div class="card mb-3">
     <?php if ($model->photo): ?>
-        <div>
+        <div class="card-img-top">
             <a href="<?= Html::encode($url) ?>">
                 <img src="<?= Html::encode($model->getThumbFileUrl('photo', 'blog_list')) ?>" alt="" class="img-responsive" />
             </a>
         </div>
     <?php endif; ?>
-    <div class="h2"><a href="<?= Html::encode($url) ?>"><?= Html::encode($model->title) ?></a></div>
-    <p><?= Yii::$app->formatter->asNtext($model->description) ?></p>
+    <div class="card-header h2">
+        <a href="<?= Html::encode($url) ?>"><?= Html::encode($model->title) ?></a>
+    </div>
+    <p class="card-body"><?= Yii::$app->formatter->asNtext($model->description) ?></p>
 </div>
 
 
