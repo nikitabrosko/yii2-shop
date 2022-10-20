62905188d18652991bafda18a6b3fad7a5c827b6
Nik.Brosko <Nikita.brosko@mail.ru>
2022-10-20 17:46:31 +0300

    Create CheckoutController

---
+++ api/controllers/user/CheckoutController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace api\controllers\user;
+
+use shop\cart\Cart;
+use shop\forms\shop\order\OrderForm;
+use shop\services\shop\OrderService;
+use Yii;
+use yii\helpers\Url;
+use yii\rest\Controller;
+use yii\web\BadRequestHttpException;
+
+class CheckoutController extends Controller
+{
+    private $cart;
+    private $service;
+
+    public function __construct($id, $module, OrderService $service, Cart $cart, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+
+        $this->cart = $cart;
+        $this->service = $service;
+    }
+
+    public function verbs(): array
+    {
+        return [
+            'index' => ['POST'],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        $form = new OrderForm($this->cart->getWeight());
+
+        $form->load(Yii::$app->request->getBodyParams(), '');
+
+        if ($form->validate()) {
+            try {
+                $order = $this->service->checkout(Yii::$app->user->id, $form);
+
+                $response = Yii::$app->getResponse();
+                $response->setStatusCode(201);
+                $response->getHeaders()->set('Location', Url::to(['shop/order/view', 'id' => $order->id], true));
+
+                return [];
+            } catch (\DomainException $e) {
+                throw new BadRequestHttpException($e->getMessage(), null, $e);
+            }
+        }
+
+        return $form;
+    }
+}
