8f7a674cc2ae5b8c9a3d876ab4ab665a66bf2b47
Nik.Brosko <Nikita.brosko@mail.ru>
2022-10-21 15:22:16 +0300

    Create DeliveryMethodReadRepository

---
+++ shop/readModels/shop/DeliveryMethodReadRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace shop\readModels\shop;
+
+use shop\entities\shop\DeliveryMethod;
+
+class DeliveryMethodReadRepository
+{
+    public function getAll(): array
+    {
+        return DeliveryMethod::find()->orderBy('sort')->all();
+    }
+}
